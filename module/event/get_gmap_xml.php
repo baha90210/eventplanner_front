c5af6232ed6471d1d8a7eaed838f91f2b14cf11c
sander <sander@sander-HP-G72-Notebook-PC>
2015-02-08 17:57:21 +0100

    Aanpassing in bestand dat de XML ophaalt uit DB. Word nu gebruik gemaakt van de instellingen in config.php en mogelijkheden van library/db.php.

--- module/event/get_gmap_xml.php
+++ module/event/get_gmap_xml.php
@@ -6,24 +6,18 @@ $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
-// Opens a connection to a MySQL server
+// Setup a db connection
+include '../../config.php';
+require_once('../../system/library/db.php');
+$db_connect = new Db();
 
-$connection=mysql_connect ('localhost', 'sander', 'sander');
-if (!$connection) {  die('Not connected : ' . mysql_error());}
-
-// Set the active MySQL database
-
-$db_selected = mysql_select_db('eventplanner', $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mysql_error());
-}
 
 // Select all the rows in the markers table
 
 $query = "SELECT * FROM location ";
 $query .= "INNER JOIN event_location as EL ON EL.location_id = location.location_id ";
 $query .= "WHERE EL.event_id = ".$_GET['event'];
-$result = mysql_query($query);
+$result = $db_connect->query($query);
 if (!$result) {
   die('Invalid query: ' . mysql_error());
 }
@@ -32,7 +26,7 @@ header("Content-type: text/xml");
 
 // Iterate through the rows, adding XML nodes for each
 
-while ($row = @mysql_fetch_assoc($result)){
+foreach ($result->rows as $row) {
   // ADD TO XML DOCUMENT NODE
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
